0bda08ffafe734b75e1312b05f9a685e9f34a79f
pmizer <mizer.piotr@gmail.com>
2019-07-30 14:55:39 +0200

    refactor to common crud interface. Closes #14

--- pet-clinic-data/src/main/java/com/pm/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/pm/springframework/petclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.pm.springframework.petclinic.services;
 
 import com.pm.springframework.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
