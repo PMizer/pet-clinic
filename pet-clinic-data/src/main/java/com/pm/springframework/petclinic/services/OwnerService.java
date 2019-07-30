dbeb2a64c7858fbcbb0cc6af5502458b94a4519a
pmizer <mizer.piotr@gmail.com>
2019-07-30 13:16:34 +0200

    Implementing Interfaces for Services. Closes #4

---
+++ pet-clinic-data/src/main/java/com/pm/springframework/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.pm.springframework.petclinic.services;
+
+import com.pm.springframework.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
